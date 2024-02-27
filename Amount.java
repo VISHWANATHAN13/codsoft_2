c459fb7ea29459274778e4eeb4169a83cafe1a40
VISHWANATHAN13 <154442800+VISHWANATHAN13@users.noreply.github.com>
2024-02-27 18:17:33 +0530

    Add files via upload

---
+++ Amount.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package home;
+
+/**
+ *
+ * @author vishwa
+ */
+public class Amount {
+    public long cash=10000;
+    public int pin=1307;
+    
+}
---
+++ MyException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package withdraw;
+
+/**
+ *
+ * @author vishwa
+ */
+public class MyException extends Exception{
+    public MyException(String s)
+    {
+        super(s);
+    }
+    
+}
